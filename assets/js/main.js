805274efa889e74f97a6d3445866d0bd35531a89
Kefelyn <kefelyn.vitoria4@gmail.com>
2026-07-07 01:24:55 -0300

    adicionando modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,26 @@ const maxRecords = 151;
 const limit = 10;
 let offset = 0;
 
-function loadPokemonItens(offset, limit) {
-    function convertPokemonToLi(pokemon) {
-        return `
-        <li class="pokemon ${pokemon.type}">
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('fecharModal');
+
+const pokemonDetails = document.getElementById('pokemonDetails');
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}"
+        data-number="${pokemon.number}"
+        data-name="${pokemon.name}"
+        data-photo="${pokemon.photo}"
+        data-type="${pokemon.type}"
+        data-types="${pokemon.types.join(',')}">
+
             <span class="number">#${pokemon.number}</span>
             <span class = "name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
                 <img src="${pokemon.photo}"
@@ -22,7 +32,9 @@ function loadPokemonItens(offset, limit) {
             </div>
         </li>
      `
-    }
+}
+
+function loadPokemonItens(offset, limit) {
 
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         //o join junta os htmls sem separação
@@ -43,9 +55,45 @@ loadMoreButton.addEventListener('click', () => {
 
         loadMoreButton.parentElement.removeChild(loadMoreButton)
     } else {
-    loadPokemonItens(offset, limit)
+        loadPokemonItens(offset, limit)
     }
 })
+
+pokemonList.addEventListener('click', (event) => {
+    const li = event.target.closest('.pokemon');
+    if (!li) return;
+
+    const name = li.dataset.name;
+    const number = li.dataset.number;
+    const photo = li.dataset.photo;
+    const type = li.dataset.type;
+    const types = li.dataset.types.split(',');
+
+    pokemonDetails.innerHTML = `
+    <div class="details ${type}">
+        <p class="number">#${number}</p>
+        <h2 class="name">${name}</h2>
+        <div class="detail">
+            <ol class="types">
+                ${types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            <img src="${photo}" alt="${name}">
+        </div>
+    </div>
+`;
+
+    modal.showModal();
+
+});
+
+closeModal.addEventListener('click', () => {
+    modal.close();
+});
+
+function showPokemonDetails(pokemon) {
+
+}
+
 
 //------ a parte de cima é tipo uma junção dessa explicação: ------
 // const newList = pokemons.map((pokemon) => {
